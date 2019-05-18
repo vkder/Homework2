479285e530f64b7b11833ee8126b99e768c0ef29
Viktor Dergatshov <viktor.dergatshov@gmail.com>
2019-05-18 14:55:58 +0300

    done

---
+++ task1.rb
@@ -0,0 +1,23 @@
+months = {
+	"January" => 31,
+	"February" => 28,
+	"March" => 31,
+	"April" => 30,
+	"May" => 31,
+	"June" => 30,
+	"July" => 31,
+	"August" => 31,
+	"September" => 30,
+	"October" => 31,
+	"November" => 30,
+	"December" => 31
+}
+
+
+months.each do |a, b|
+
+puts "30 дней в следующих месяцах #{a}" if b == 30
+	
+end
+
+   
---
+++ task2.rb
@@ -0,0 +1,6 @@
+array1 = [10]
+i = array1[0]
+while i <= 100
+	puts i
+	i += 5
+end
