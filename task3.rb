bdb4ef12e3a8c96ffe8a8ccba7dee50ae1eca351
Viktor Dergatshov <50587324+vkder@users.noreply.github.com>
2019-05-26 13:54:37 +0300

    totally changed

--- task3.rb
+++ task3.rb
@@ -1,11 +1,9 @@
-new_arr = [0, 1]
-i = 0
+fib = [0,1]
 
-while new_arr.last <= 100 
-  fibonacci = new_arr[i] + new_arr[i.next]
-  new_arr << fibonacci
-  i += 1
-  puts new_arr[i]
+loop do
+  next_fib = fib[-1] + fib[-2] 
+  break if next_fib >= 100
+  fib << next_fib
 end
 
-
+puts fib
