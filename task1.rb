bee2fb12f29a7670980bfb23c93ff8eaf34a7117
Viktor Dergatshov <50587324+vkder@users.noreply.github.com>
2019-05-26 13:30:34 +0300

    Add files via upload

--- task1.rb
+++ task1.rb
@@ -1,23 +1,20 @@
 months = {
-	"January" => 31,
-	"February" => 28,
-	"March" => 31,
-	"April" => 30,
-	"May" => 31,
-	"June" => 30,
-	"July" => 31,
-	"August" => 31,
-	"September" => 30,
-	"October" => 31,
-	"November" => 30,
-	"December" => 31
+  "January" => 31,
+  "February" => 28,
+  "March" => 31,
+  "April" => 30,
+  "May" => 31,
+  "June" => 30,
+  "July" => 31,
+  "August" => 31,
+  "September" => 30,
+  "October" => 31,
+  "November" => 30,
+  "December" => 31
 }
 
-
 months.each do |month, day|
-
 puts "30 дней в следующих месяцах #{month}" if day == 30
-	
 end
 
    
